c6b70d429cb9edb5bf06502a0edf9a672c25f063
huyen-seatek <huyen.ho@seatek.vn>
2025-07-24 22:32:14 +0700

    Fix: Set Cookie lại cho việc lưu kho đang được chọn

--- wp-content/plugins/WooCommerce-Multi-Locations-Inventory-Management/public/js/wcmlim_utility/generic/wcmlim_setcookies.js
+++ wp-content/plugins/WooCommerce-Multi-Locations-Inventory-Management/public/js/wcmlim_utility/generic/wcmlim_setcookies.js
@@ -1,11 +1,13 @@
 export function setcookie(name, value, days) {
   let date = new Date();
-  if (days) {
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    var expires = `; expires=${date.toUTCString()}`;
-  } else {
-    date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000);
-    var expires = `; expires=${date.toUTCString()}`;
-  }
+  // if (days) {
+  //   date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  //   var expires = `; expires=${date.toUTCString()}`;
+  // } else {
+  //   date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+  //   var expires = `; expires=${date.toUTCString()}`;
+  // }
+  date.setTime(date.getTime() + 10 * 365 * 24 * 60 * 60 * 1000); 
+  var expires = `; expires=${date.toUTCString()}`;
   document.cookie = `${name}=${value}${expires};path=/`;
 }
